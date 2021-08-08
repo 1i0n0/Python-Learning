8cafbca12e2bcb5fdc2630be6faec88be976d099
Eric Chen <85225837+1i0n0@users.noreply.github.com>
2021-08-08 22:10:06 +0800

    Add files via upload

---
+++ practices/circle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding = UTF-8 -*-
+from math import pi
+
+
+def calc_circumference(r):
+    return 2.0 * pi * r
+
+
+def calc_area(r):
+    return pi * r ** 2
+
+
+if __name__ == "__main__":
+    try:
+        radius = float(input("What is the radius of your circle?\n"))
+    except ValueError:
+        print("Please enter a number.")
+    else:
+        assert radius > 0
+        circumference = calc_circumference(radius)
+        area = calc_area(radius)
+        print("circumference = {0:.2f}, area = {1:.2f}".format(circumference, area))
