9a7883b2a77d13a39e9e726f2ebd597e87254980
Eric Chen <85225837+1i0n0@users.noreply.github.com>
2021-08-08 20:17:26 +0800

    Add files via upload

---
+++ practices/fathm_ft.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+def fathoms2feet(fathoms):
+    return 6 * fathoms
+
+
+if __name__ == "__main__":
+    try:
+        fathoms = int(input("Fathom(s): "))
+    except ValueError:
+        print("Please enter an integer.")
+    else:
+        print("There are {0} feet in {1} fathom(s).".format(fathoms2feet(fathoms), fathoms))
