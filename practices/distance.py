cf00f43bf722f61fe70bd1339b29ffb8b8bdc415
Eric Chen <85225837+1i0n0@users.noreply.github.com>
2021-08-09 22:22:01 +0800

    Add files via upload

---
+++ practices/distance.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding = UTF-8 -*-
+
+# Compute the distance between the two points
+import sys
+from math import sqrt
+
+
+def calc_distance():
+    try:
+        # (x1, y1)
+        x1 = float(input("x1: "))
+        y1 = float(input("y1: "))
+
+        # (x2, y2)
+        x2 = float(input("x2: "))
+        y2 = float(input("y2: "))
+
+        distance = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+        print("Distance: {0:.2f}".format(distance))
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    calc_distance()
