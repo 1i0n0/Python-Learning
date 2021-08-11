eb28f30971e099e21fe8e095a38767ff65ca46e2
Eric Chen <85225837+1i0n0@users.noreply.github.com>
2021-08-11 22:09:27 +0800

    Add files via upload

---
+++ practices/guess.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding = UTF-8 -*-
+
+# Play a simple guessing game with the user
+
+import random
+
+if __name__ == "__main__":
+    max_num = 10
+    answer = random.randrange(1, max_num)
+
+    print(f"I'm thinking of a number between 1 and {max_num}.")
+
+    while True:
+        try:
+            guess = int(input("Guess what it is: "))
+        except ValueError:
+            print("This is not a number. Please try again or enter 0 to exit.")
+            continue
+        else:
+            if guess == 0:
+                print(f"The number was {answer}.")
+                break
+            elif guess == answer:
+                print("You got it! Good guessing!")
+                break
+            else:
+                print("Sorry, that is not correct. Please try again or enter 0 to exit.")
+                continue
+    
