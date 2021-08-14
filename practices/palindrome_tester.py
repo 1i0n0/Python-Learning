39b713ad445b4b2fd223111583f02f4554664644
Eric Chen <85225837+1i0n0@users.noreply.github.com>
2021-08-14 22:56:31 +0800

    Add files via upload

---
+++ practices/palindrome_tester.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding = UTF-8 -*-
+
+# Test a string to see if it is a palindrome
+def is_palindrome(s):
+    s.strip()
+    left = 0
+    right = len(s) - 1
+
+    while s[left] == s[right] and left < right:
+        left += 1
+        right -= 1
+
+    if left < right:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    _str = input("Enter a potential palindrome: ")
+    if is_palindrome(_str):
+        print("That string IS a palindrome.")
+    else:
+        print("That string IS NOT a palindrome.")
